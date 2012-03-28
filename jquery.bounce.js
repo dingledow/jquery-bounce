7cbaff9753e5ad45956702785e43968a8f6cd4e8
Gary Hepting <gary@emergeinteractive.com>
2012-03-28 13:47:59 -0700

    working for multiple objects and configurations

--- jquery.bounce.js
+++ jquery.bounce.js
@@ -8,11 +8,12 @@
 			$.fn.bounce = function(options) {
 
 				var defaults = {
-					distance: '25px', // distance to move
-					downspeed: 450, // speed to move to ending point
-					upspeed: 200, // speed to return to starting point
+					id: $(this).attr('id'),
+					distance: '20px', // distance to move
+					downspeed: 300, // speed to move to ending point
+					upspeed: 300, // speed to return to starting point
 					gravity: 'bottom', // direction to move object (top or bottom)
-					timer: 650, // delay to repeat bounce (should be minimum of downspeed + upspeed)
+					timer: 600, // delay to repeat bounce (should be minimum of downspeed + upspeed)
 					loop: 0, // number of times to loop bouncing animation
 					easingdown: 'easeInExpo', // easing to ending point
 					easingup: 'jswing' // easing back to starting point
@@ -22,24 +23,28 @@
 					defaults.timer = defaults.downspeed+defaults.upspeed;
 				}
 
-				if (options == 'stop') {
-					clearInterval(obj.interval);
-					return false;
-				}else if (options == 'start') {
-					return this.each(function() {
-						obj = $(this);
-						obj.interval = setInterval(function() {
-							obj.stop().animate({'margin-top': (defaults.gravity == 'top' ? '-' : '')+defaults.distance, 'opacity': '1'}, defaults.downspeed, defaults.easingdown).animate({'margin-top': '0', 'opacity': '1'}, defaults.upspeed, defaults.easingup);
-						}, defaults.timer);
-	    			});
-				}
+				// if (options == 'stop') {
+				// 	clearInterval(obj.interval);
+				// 	return false;
+				// }else if (options == 'start') {
+				// 	return this.each(function() {
+				// 		obj = $(this);
+				// 		obj.interval = setInterval(function() {
+				// 			obj.stop().animate({'margin-top': (defaults.gravity == 'top' ? '-' : '')+defaults.distance, 'opacity': '1'}, defaults.downspeed, defaults.easingdown).animate({'margin-top': '0', 'opacity': '1'}, defaults.upspeed, defaults.easingup);
+				// 		}, defaults.timer);
+				// 	});
+				// }
 
 				var options = $.extend(defaults, options);
-				
-				return this.each(function() {
+				var interval = [], timeout = [];
+
+				return this.each(function(obj) {
 					obj = $(this);
+					obj.wrap('<div class="container" />');
+					obj.css('position','absolute');
 					obj.interval = setInterval(function() {
 						obj.stop().animate({'margin-top': (defaults.gravity == 'top' ? '-' : '')+defaults.distance, 'opacity': '1'}, defaults.downspeed, defaults.easingdown).animate({'margin-top': '0', 'opacity': '1'}, defaults.upspeed, defaults.easingup);
+						console.log();
 					}, defaults.timer);
 					if(defaults.loop > 0) {
 						obj.timeout = setTimeout(function() {
